f536d34e624ca6662a60dbee43ce88e885c30270
Glitch (jessicas-wishlist) <none>
2017-12-11 07:55:32 +0000

    🦃🐓 Checkpoint wishlist.yml:588064 components/Thing.js:588064

--- components/Thing.js
+++ components/Thing.js
@@ -1,23 +1,40 @@
 import { h } from 'preact'; /** jsx h */
 
+const renderLinks = (links) => links.reduce((acc, url, number) => {
+  const { hostname } = new URL(url);
+  return acc.join([
+    <a key={a} href={url}>
+      {hostname}
+    </a>
+  ]);
+}, [])
+
 export default ({thing}) => {
-  let heading = (
+  let content = (
     <strong>
       {thing.name}
     </strong>
   );
 
   if (thing.link) {
-    heading = (
-      <a href={thing.link}>
-        {heading}
-      </a>
-    );
+    if (Array.isArray(thing.link)) {
+      content = (
+        <span>
+          {content} ({})
+        </span>
+      );
+    } else {
+      content = (
+        <a href={thing.link}>
+          {content}
+        </a>
+      );
+    }
   }
 
   return (
     <li>
-      {heading}
+      {content}
       {thing.notes && (
         <p style="margin: 0">
           {thing.notes}
