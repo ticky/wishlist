ef8607e8b36d9e54f267b725e81ef45d8c98e462
Glitch (square-science) <none>
2017-12-11 05:58:01 +0000

    🐸💂 Checkpoint src/App.js:588064 components/pages/home.js:588064 wishlist.yml:588064

--- components/pages/home.js
+++ components/pages/home.js
@@ -1,12 +1,26 @@
 import { h, Component } from 'preact'; /** jsx h */
 import { items, wishlists } from '../../wishlist.yml';
 
+const Wishitem = ({item}) => (
+  <li>
+    <a href={item.link}>{item.name}</a><br/>
+    {JSON.stringify(item, null, '  ')}
+  </li>
+);
+
 export default class App extends Component {
   render() {
     return (
       <div>
         <h1>{`Jessica's meta-wishlist`}</h1>
-        <pre>{JSON.stringify({items, wishlists})}</pre>
+        <h2>Other Lists</h2>
+        <ul>
+          {wishlists.map((item) => <Wishitem item={item} />)}
+        </ul>
+        <h2>Things I Want</h2>
+        <ul>
+          {items.map((item) => <Wishitem item={item} />)}
+        </ul>
       </div>
     );
   }
